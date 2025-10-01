f648fca09575d86385d45cdbe6b1b52970e2c480
dr1nkz <raptorpower1@gmail.com>
2025-10-01 21:16:51 +0300

    fixed mqtt message sending

--- pigs_counter/mqtt_notificator.py
+++ pigs_counter/mqtt_notificator.py
@@ -9,14 +9,14 @@ MQTT_NOTIFICATIONS_TOPIC = os.getenv(
     'MQTT_NOTIFICATIONS_TOPIC', 'pigs_unloading')
 
 
-def send_mqtt_message(pigs_quantity: int, start_time: str, end_time: str, platenumber: str = 'None', place: str = 'None', pigs_defect: int = 0):
+def send_mqtt_message(pigs_quantity: int, start_time: str, end_time: str, truck_id: str = 'None', place: str = 'None', pigs_defect: int = 0):
     # Формируем payload для MQTT
     payload = {
         'title': 'Разгрузка',
         'start_time': start_time,
         'end_time': end_time,
         'pigs_quantity': pigs_quantity,
-        'platenumber': platenumber,
+        'truck_id': truck_id,
         'pigs_defect': pigs_defect,
         'place': place,
     }
